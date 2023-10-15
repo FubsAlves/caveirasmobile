205cec22cb9818ecdd0e3812f1a20a6bd36f054f
Fabiano Alves <fabianoxtx@gmail.com>
2023-10-15 03:38:59 -0300

    ResizeMode for Carousel

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -45,7 +45,7 @@ export default function CarouselNews() {
                         key={item.id}   
                     >
                         {console.log(item)}
-                        <Image w="100%" h="100%" source={{uri: item.image.url}} alt="news"/>
+                        <Image w="100%" h="100%" source={{uri: item.image.url}} resizeMode="stretch" alt="news"/>
                     </Flex>
                 )}
             />
