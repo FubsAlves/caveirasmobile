726a8ccb5d042458433f08d800173014ddb32fcb
Fabiano Alves <fabianoxtx@gmail.com>
2023-10-16 02:15:01 -0300

    carousselscreen removal

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -82,6 +82,7 @@ export default function Menu() {
         }}> 
         <VStack space={0} w="20%" h="90%"alignItems="center">
             <Flex>
+              
               {route.name === "Chickens" ? (
                 <Image source={require("../../assets/images/chickens-logo.webp")} w={9} h={9} alt="chickens-logo" />
               ) : (
