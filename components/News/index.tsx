e2ac8c360281ee79e15bc3abce7b5e0091746133
Fabiano Alves <fabianoxtx@gmail.com>
2023-10-16 00:37:22 -0300

    Testing news image size

--- components/News/index.tsx
+++ components/News/index.tsx
@@ -19,8 +19,24 @@ export default function News() {
                 console.log("Loading")
             ) : 
             (
+                <>
+                    <Flex my={3} w={dimensions.width} h={dimensions.height * 0.35}>
+                        <Image w="100%" h="100%" zIndex={3} source={{uri: "https://media.graphassets.com/922iJxu5TayXnJbIGma7"}} resizeMode="stretch" alt="News"/>
+                        {/* <Flex w="100%" justifyContent="center" h="25%" bottom="0%" zIndex={4} position="absolute" opacity={0.7}
+                            bg={{
+                                linearGradient: {
+                                    colors: ["gray.400", "black"],
+                                    locations: [0.5, 0.5]
+                                }
+                            }}
+                        >
+                            <Text>O mais novo da família</Text>
+                        </Flex> */}
+                    </Flex>
+                    <Flex my={3} bgColor="cyan.800" w={dimensions.width} h={dimensions.height * 0.35}></Flex>
+                    {console.log(data.newss)}
+                </>
                 
-                console.log(data)
             )}
       
         </Flex>
