9e08a3fd87d875aed26556619d3df87e643326b3
Fabiano Alves <fabianoxtx@gmail.com>
2023-10-16 02:20:02 -0300

    Header component to Newsscreen

--- screens/newsscreen.tsx
+++ screens/newsscreen.tsx
@@ -2,6 +2,7 @@ import { Flex, Image, ScrollView, StatusBar, Text } from "native-base";
 import Menu from "../components/Menu";
 import CarouselNews from "../components/Carousel";
 import News from "../components/News";
+import Header from "../components/Header";
 
 export default function NewsScreen () {
     return(
@@ -9,12 +10,7 @@ export default function NewsScreen () {
         <StatusBar hidden />
             <Flex w="100%" h="90%" backgroundColor="caveirito.100" justifyContent="center" alignItems="center">
                 <Flex shadow="3" borderRadius="3xl" borderBottomRadius="none" w="100%" h="100%" backgroundColor="#FFFFFF">
-                    <Image
-                        source={require('../assets/images/caveiras-logo.png')}
-                        alt="Caveiras Logo"
-                        h="70px"
-                        w="70px"
-                        alignSelf="center" />
+                    <Header/>
                     <ScrollView indicatorStyle="black">
                         <CarouselNews/>
                         <News/>
