5473209e9913f30c4b2791c650b9a87eae4e6687
Fabiano Alves <fabianoxtx@gmail.com>
2023-10-20 23:23:36 -0300

    Lottie Animation

--- screens/menuscreen.tsx
+++ screens/menuscreen.tsx
@@ -1,11 +1,11 @@
 import Menu from "../components/Menu";
 import Header from "../components/Header";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, Suspense } from "react";
 import { Flex, Text, Image, HStack } from "native-base";
 import { View, Dimensions, TouchableWithoutFeedback } from "react-native";
 import Loading from "../components/Loading";
 import GET_SNACKS from "../queries/snacks"
-import { useQuery } from "@apollo/client";
+import { useQuery, useSuspenseQuery } from "@apollo/client";
 import { ImageZoom } from "@likashefqet/react-native-image-zoom";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Animated, { FadeInDown } from "react-native-reanimated";
@@ -16,7 +16,7 @@ interface ItemProps {
   id: number;
   name: string;
   logoSrc?: ImageSource;
-  secondaryLogoSrc?: ImageSource; 
+  secondaryLogoSrc?: ImageSource;
   imageSrc?: ImageSource;
   description: string;
   price: string;
@@ -27,14 +27,13 @@ interface ImageSource {
   url: string;
 }
 
-export default function MenuScreen () {
-  
+export default function MenuScreen() {
+
   const route = useRoute();
   const params = route.params.name;
   const animation = useRef(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const {loading, error, data} = useQuery(GET_SNACKS, {
-    variables: {params},
+  const { error, data } = useSuspenseQuery(GET_SNACKS, {
+    variables: { params },
   });
 
 
@@ -42,20 +41,13 @@ export default function MenuScreen () {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
   }
-  
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1200);
-  }, []);
-
 
   const renderItem = ({ item }: { item: ItemProps }) => (
     <>
-      <GestureHandlerRootView style={{width: dimensions.width, height: "100%", alignItems:"center", justifyContent: "center"}}>
-      
-      <LottieView style={{position: "absolute", zIndex: 1001, width: 60, height: 60, top: "15%", left: "10%"}} autoPlay loop={true} ref={animation} source={require('../assets/animation/newSnack.json')}/>
-      {<HStack space={6}  mt={2} alignItems="center">
+      <GestureHandlerRootView style={{ width: dimensions.width, height: "100%", alignItems: "center", justifyContent: "center" }}>
+
+        {<HStack space={6} mt={4} alignItems="center">
+        
           {item.logoSrc &&
             <Image
               source={{
@@ -65,42 +57,43 @@ export default function MenuScreen () {
               h={12}
               w={12}
             />
-            }
+          }
+          <LottieView style={{width: 55, height: 55 }} autoPlay loop={true} ref={animation} source={require('../assets/animation/newSnack.json')} />
 
           {item.secondaryLogoSrc &&
-          
-          <Image
-            source={{
-              uri: item.secondaryLogoSrc.url,
-            }}
-            alt={item.name}
-            h={12}
-            w={12}
-          />
-          
+
+            <Image
+              source={{
+                uri: item.secondaryLogoSrc.url,
+              }}
+              alt={item.name}
+              h={12}
+              w={12}
+            />
+
           }
         </HStack>}
+        
         <Text color="#502314" fontWeight={600} fontSize={20}>
-            {item.name}
-            
+          {item.name}
         </Text>
-        <TouchableWithoutFeedback onPress={() => {}}>
-        
-        <Flex zIndex={1000} mt={4} width={dimensions.width * 0.8} height={dimensions.height * 0.4}>
-          
-          <ImageZoom
-            
-            style={{
-              width: dimensions.width * 0.8,
-              height: dimensions.height * 0.4,
-            }}
-            source={{
-              uri: item.imageSrc.url,
-            }}
-            alt={item.name}
-          />
-        </Flex>
-        
+        <TouchableWithoutFeedback onPress={() => { }}>
+
+          <Flex zIndex={1000} mt={4} width={dimensions.width * 0.8} height={dimensions.height * 0.4}>
+
+            <ImageZoom
+
+              style={{
+                width: dimensions.width * 0.8,
+                height: dimensions.height * 0.4,
+              }}
+              source={{
+                uri: item.imageSrc.url,
+              }}
+              alt={item.name}
+            />
+          </Flex>
+
         </TouchableWithoutFeedback>
         <Text color="#502314" w="90%" mt={2} fontSize="12" textAlign="center">
           {item.description}
@@ -110,16 +103,9 @@ export default function MenuScreen () {
         </Text>
       </GestureHandlerRootView>
     </>
-    
+
   );
 
-
-
-  const renderLoading = () => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Loading />
-    </View>
-  );
 
   return (
     <>
@@ -127,9 +113,8 @@ export default function MenuScreen () {
 
       <Flex w="100%" alignItems="center" h="72%" bgColor="#FFFFFF">
         <View style={{ flex: 1, flexGrow: 1, flexDirection: "column" }}>
-          {isLoading ? (
-            renderLoading()
-          ) : (
+
+          <Suspense fallback={<Loading />}>
             <Animated.FlatList
               entering={FadeInDown.duration(500)}
               data={data.snacks}
@@ -143,10 +128,10 @@ export default function MenuScreen () {
               maxToRenderPerBatch={1} // Number of items to render in each batch
               windowSize={3} // Number of items in the visible window
             />
-          )}
+          </Suspense>
         </View>
       </Flex>
-      <Menu/>
+      <Menu />
     </>
   );
 }
