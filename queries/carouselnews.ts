3c02f0bf103433ff04c2a7f8352fc88f43c2efcc
Fabiano Alves <fabianoxtx@gmail.com>
2023-10-22 20:00:04 -0300

    Newss query by update Desc

--- queries/carouselnews.ts
+++ queries/carouselnews.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 const GET_CAROUSELNEWS = gql`
     query CarouselNews {
-        newss(where: {isCarouselItem: true}) {
+        newss(where: { isCarouselItem: true }, orderBy: updatedAt_DESC) {
         id
         image {
             url
--- queries/news.ts
+++ queries/news.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 const GET_NEWS = gql`
     query CarouselNews {
-        newss(where: {isCarouselItem: false}) {
+        newss(where: { isCarouselItem: false }, orderBy: updatedAt_DESC) {
         id
         image {
             url
